b8249cc4c93ef2d1ea66b41ef63b5a6d38659ba0
Mattheus <mattheuseinloft@gmail.com>
2024-12-27 13:09:08 -0300

    chore: fix release branch name

--- release.config.mjs
+++ release.config.mjs
@@ -2,7 +2,7 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 export default {
-  branches: ["master", "next"],
+  branches: ["main", "next"],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
